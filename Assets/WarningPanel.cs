434a457a2927ad0570c4bd55a70fba28d2d2bceb
hxxler <mrlongstrong2003@gmail.com>
2023-06-04 20:05:38 +0300

    RCube
    RCube

---
+++ Assets/WarningPanel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarningPanel : MonoBehaviour
+{
+    private const string FirstTimeKey = "FirstTime";
+
+    private void Start()
+    {
+        bool isFirstTime = PlayerPrefs.GetInt(FirstTimeKey, 1) == 1;
+        if (isFirstTime)
+        {
+            // Показать панель
+            gameObject.SetActive(true);
+            // Установить флаг, что игра запущена в первый раз
+            PlayerPrefs.SetInt(FirstTimeKey, 0);
+        }
+        else
+        {
+            // Скрыть панель
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void RemovePanel()
+    {
+        // Скрыть панель
+        gameObject.SetActive(false);
+    }
+}
